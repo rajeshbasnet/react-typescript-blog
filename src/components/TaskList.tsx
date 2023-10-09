68db613d708fb8ea953df377a8cd4065563f3f7d
rajeshbasnet <rajeshkhapataribasnet123@gmail.com>
2023-10-09 19:58:22 +0545

    Added remove task functionality

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
-import { StateProps } from "../redux/slice";
+import { StateProps, removeTask } from "../redux/slice";
+import { useDispatch } from "react-redux";
 
 type SelectorProps = {
   task: StateProps;
@@ -8,11 +9,44 @@ type SelectorProps = {
 const TaskList = () => {
   const taskList = useSelector((state: SelectorProps) => state.task.value);
 
+  const dispatch = useDispatch();
+
+  function deleteTaskHandler(index: number) {
+    dispatch(removeTask(index));
+  }
+
   return (
     <section className="task__list__section">
-      <ul>
+      <ul
+        style={{
+          listStyleType: "none",
+        }}
+      >
         {taskList.map((item, index) => {
-          return <li key={index}>{item}</li>;
+          return (
+            <div
+              key={index}
+              style={{
+                margin: "2rem 0",
+                border: "2px solid blue",
+                padding: "1rem",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-around",
+              }}
+            >
+              <li>{item}</li>
+              <button
+                onClick={() => deleteTaskHandler(index)}
+                style={{
+                  backgroundColor: "red",
+                  color: "white",
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          );
         })}
       </ul>
     </section>
