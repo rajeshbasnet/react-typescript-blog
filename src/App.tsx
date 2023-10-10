87ff8e8230cf81eefbfa10f3a8a4ede579652c12
rajeshbasnet <rajeshkhapataribasnet123@gmail.com>
2023-10-10 12:52:23 +0545

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { Provider } from "react-redux";
 import "./App.css";
-import TaskForm from "./components/TaskForm";
-import store from "./redux/store";
 
 function App() {
-  return (
-    <>
-      <Provider store={store}>
-        <TaskForm />
-      </Provider>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/redux/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import taskReducer from "./slice";
-
-export default configureStore({
-  reducer: {
-    task: taskReducer,
-  },
-});
